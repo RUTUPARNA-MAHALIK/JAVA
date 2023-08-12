a28b0f6072d2b22b2bf374b584adf4912ab349fb
RUTUPARNA MAHALIK <107238378+RUTUPARNA-MAHALIK@users.noreply.github.com>
2023-08-12 19:54:25 +0530

    Add files via upload

---
+++ arithmeticoperation.java
@@ -0,0 +1,11 @@
+class arithmeticoperation
+{
+public static void main(String args[ ])
+{ int a=13, b=4; 
+System.out.println("Addition = "+ (a+b));
+System.out.println("Subtraction = "+ (a-b)); 
+System.out.println("Product = "+ (a*b)); 
+System.out.println("Quotient = "+ (a/b)); 
+System.out.println("Remainder = "+ (a%b));
+}
+}
